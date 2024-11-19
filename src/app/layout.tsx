5613e098a244aaf4a955ce26620f80660ce57aa7
Satya Prakash Mishra <kali69@MiWiFi-R4CM-srv.stya's>
2024-11-19 19:08:57 +0530

    Added page.tsx to website folder and created layout.tsx in website folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/theme";
 
 const manrope = Manrope({subsets: ['latin']})
 
@@ -16,12 +17,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
     <ClerkProvider>
-      <body
-        className={`${manrope.className} bg-[#171717]`}>{children}
+    <html lang="en">
+      <body className={`${manrope.className} bg-[#171717]`}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+      </ThemeProvider>
       </body>
-    </ClerkProvider>
     </html>
-  );
+    </ClerkProvider>
+  )
 }
